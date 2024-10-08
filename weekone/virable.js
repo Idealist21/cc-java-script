273b0c49b3ae33d33bbee47afe431ab439ef84ac
idealist21 <oyiowo52@gmail.com>
2024-10-08 06:20:42 -0700

    learn about variables and operators

---
+++ weekone/virable.js
@@ -0,0 +1,54 @@
+// alert('hello');
+// this is
+// a
+// multiline
+// comment
+/*
+this is 
+a mulitiline 
+comment 
+*/
+
+let fname = 'success';
+// alert(fname);
+fname = 'first place';
+// alert(fname);
+
+const surname = 'adebayo';
+// alert(surname);
+// surname ='musa';
+// alert(surname);
+
+let firstNum = 5;
+let secondNum= 2;
+let add = firstNum + secondNum;
+console.log( "5 + 2 =" + add);
+let subtract = firstNum - secondNum;
+let divide = firstNum / secondNum;
+let multiply = firstNum * secondNum;
+console.log("5 - 2 =" + subtract);
+console.log("5 / 2 =" + divide);
+console.log("5 * 2 =" + multiply);
+let power = firstNum ** secondNum;
+console.log("5 raised to power 2 ="+ power);
+let remainder = firstNum % secondNum;
+console.log("5 divided by 2 is 2 r " + remainder);
+console.log("5 > 2 =" + (firstNum > secondNum));
+console.log("5 < 2 =" + (firstNum < secondNum));
+console.log("5 >= 2 =" + (firstNum >= secondNum));
+console.log("5 <= 2 =" + (firstNum <= secondNum));
+console.log("5 is equal to 5" + (firstNum == firstNum));
+console.log("5" == 5);
+console.log("5" === 5);
+console.log("5 is not equal to 2" + (firstNum =!  secondNum));
+
+let colorofTop = "white";
+let colorofLegs = "ash";
+let numberofLeg ="4";
+let lenght = 1.5;
+let isfoldabel = true;
+let materialoftop = "plastic";
+let materialofLegs = "aluminum";
+let isheavy = false;
+let texture ="coarse";
+let noofTabels = 6;
